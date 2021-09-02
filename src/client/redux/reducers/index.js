767e67829c9150b70787484ad97e20ed0b7e5e29
Mikhail <pmp@nopreset.ru>
2021-09-02 02:28:15 -0400

    auth pages

--- src/client/redux/reducers/index.js
+++ src/client/redux/reducers/index.js
@@ -3,8 +3,11 @@ import { reducer as formReducer } from "redux-form";
 import { routerReducer } from "connected-react-router";
 import { appReducer } from "./appReducer";
 import { connectRouter } from "connected-react-router";
+import { authReducer } from "./authReducer";
 
 export default (history) => combineReducers({
-	router: connectRouter(history),
-	app: appReducer,
+    router: connectRouter(history),
+    form: formReducer,
+    app: appReducer,
+    auth: authReducer,
 })
