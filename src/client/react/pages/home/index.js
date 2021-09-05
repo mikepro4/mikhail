085fe02c422cf5351238614dfbd0b584a86caebb
Mikhail <pmp@nopreset.ru>
2021-09-04 23:59:21 -0400

    another zone

--- src/client/react/pages/home/index.js
+++ src/client/react/pages/home/index.js
@@ -167,6 +167,48 @@ class HomePage extends Component {
         })
     }
 
+    handleZone7TouchStart = (event) => {
+        if(!this.state.touched) {
+            console.log(event)
+            this.setState({
+                touched: true,
+                activeZone: 7
+            })
+            this.startZone7TimeInterval()
+        }
+    }
+
+    handleZone7TouchEnd= (event) => {
+        clearInterval(this.state.timeInterval);
+
+        this.setState({
+            touched: false,
+            time: 0,
+            activeZone: null
+        })
+    }
+
+    handleZone8TouchStart = (event) => {
+        if(!this.state.touched) {
+            console.log(event)
+            this.setState({
+                touched: true,
+                activeZone: 8
+            })
+            this.startZone8TimeInterval()
+        }
+    }
+
+    handleZone8TouchEnd= (event) => {
+        clearInterval(this.state.timeInterval);
+
+        this.setState({
+            touched: false,
+            time: 0,
+            activeZone: null
+        })
+    }
+
     handleZone5TouchStart = (event) => {
         if(!this.state.touched) {
             console.log(event)
@@ -432,7 +474,89 @@ class HomePage extends Component {
 		}, 1);
 
 		this.setState({ timeInterval });
-	}
+    }
+    
+    startZone7TimeInterval() {
+        clearInterval(this.state.timeInterval);
+
+        let selectedShape
+
+        if(this.props.shape.newShape.defaultViz) {
+            selectedShape = this.props.shape.newShape
+        } else {
+            selectedShape = this.props.shape.currentShape
+        }
+        this.setState({
+            originalBoldRate: selectedShape.defaultViz.shape.boldRate 
+        })
+
+		const timeInterval = setInterval(() => {
+            this.setState({ 
+                time: this.state.time + 100
+             });
+
+            //  let newShape = this.props.shape.currentShape.defaultViz.shape
+            
+            // console.log(selectedShape)
+            if(selectedShape && selectedShape.defaultViz) {
+                let finalshape = {
+                    ...selectedShape,
+                    defaultViz: {
+                       ...selectedShape.defaultViz,
+                       shape: {
+                           ...selectedShape.defaultViz.shape,
+                           rotateSpeed: selectedShape.defaultViz.shape.rotateSpeed  + this.state.time / 100000
+                       }
+                    }
+                }
+                this.props.loadNewShape(finalshape)
+            }
+             
+		}, 1);
+
+		this.setState({ timeInterval });
+    }
+    startZone8TimeInterval() {
+        clearInterval(this.state.timeInterval);
+
+        let selectedShape
+
+        if(this.props.shape.newShape.defaultViz) {
+            selectedShape = this.props.shape.newShape
+        } else {
+            selectedShape = this.props.shape.currentShape
+        }
+        this.setState({
+            originalBoldRate: selectedShape.defaultViz.shape.boldRate 
+        })
+
+		const timeInterval = setInterval(() => {
+            this.setState({ 
+                time: this.state.time + 100
+             });
+
+            //  let newShape = this.props.shape.currentShape.defaultViz.shape
+            
+            // console.log(selectedShape)
+            if(selectedShape && selectedShape.defaultViz) {
+                let finalshape = {
+                    ...selectedShape,
+                    defaultViz: {
+                       ...selectedShape.defaultViz,
+                       shape: {
+                           ...selectedShape.defaultViz.shape,
+                           rotateSpeed: selectedShape.defaultViz.shape.rotateSpeed  - this.state.time / 100000
+                       }
+                    }
+                }
+                this.props.loadNewShape(finalshape)
+            }
+             
+		}, 1);
+
+		this.setState({ timeInterval });
+    }
+    
 
 
 
@@ -480,6 +604,23 @@ class HomePage extends Component {
                                 }, "touch-zone touch-zone-4")}
                                 onTouchStart={(event) => this.handleZone4TouchStart(event)}
                                 onTouchEnd={(event) => this.handleZone4TouchEnd(event)}
+                            ></div>
+                        </div>
+
+                        <div className="touch-zones-row touch-zones-row-3">
+                            <div 
+                                className={classNames({
+                                    "touch-zone-active": this.state.activeZone == 7
+                                }, "touch-zone touch-zone-7")}
+                                onTouchStart={(event) => this.handleZone7TouchStart(event)}
+                                onTouchEnd={(event) => this.handleZone7TouchEnd(event)}
+                            ></div>
+                            <div 
+                                className={classNames({
+                                    "touch-zone-active": this.state.activeZone == 8
+                                }, "touch-zone touch-zone-8")}
+                                onTouchStart={(event) => this.handleZone8TouchStart(event)}
+                                onTouchEnd={(event) => this.handleZone8TouchEnd(event)}
                             ></div>
                         </div>
 
